f13acb8fe710027f8a252d1343bd0b174dc6195a
15Akhila <83770102+15Akhila@users.noreply.github.com>
2021-05-11 20:39:01 +0530

    My Exercise

--- app/src/main/java/com/example/kotlindemo/main.kt
+++ app/src/main/java/com/example/kotlindemo/main.kt
@@ -1,5 +1,9 @@
 package com.example.kotlindemo
 
 fun main(){
-      print("welcome to my first kotlin")
+      var mystring:String="Andriod kotlin"
+      var myfloat:Float=13.37f
+      val mydouble:Double=3.14159265358979
+      var myint:Int=25
+      print("$mystring \n $myfloat \n $mydouble \n $myint")
 }
